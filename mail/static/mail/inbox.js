87c463048d237befc86a75e900d6e6d05cda22b8
boshhy <boshhy101@yahoo.com>
2022-01-30 17:09:05 -0800

    before adding styling to buttons on view email page

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -47,16 +47,42 @@ function load_mailbox(mailbox) {
         const div = document.createElement('div');
         console.log(email);
         if (email['read'] === true) {
-          div.style.backgroundColor = "#aaaaaa";
+          div.style.backgroundColor = "#D3D3D3";
         }
-        sender = email["sender"];
+        div.addEventListener("mouseover", () => {
+          div.style.cursor = 'pointer';
+          div.style.backgroundColor = '#afcfee';
+        })
+        div.addEventListener("mouseout", () => {
+          if (email['read'] === true) {
+            div.style.backgroundColor = "#D3D3D3";
+          }
+          else {
+            div.style.backgroundColor = '#ffffff'
+          }
+        })
+        if (mailbox === 'sent') {
+          sender = email['recipients']
+        }
+        else {
+          sender = email["sender"];
+        }
         recipients = email['recipients'];
         subject = email['subject'];
         time_sent = email['timestamp'];
-        div.innerHTML = 'Sender: ' + sender + ' --- recipients: ' + recipients + ' --- Subject: ' + subject + ' --- Date: ' + time_sent;
         div.onclick = () => { display_email(email['id'], mailbox) }
         div.style.border = '1px solid black';
         div.style.margin = '8px';
+        div.style.padding = '10px';
+        div.style.borderRadius = '16px';
+        div.innerHTML = `<span style="width: 250px;
+                    margin-left: 8px;
+                    display: inline-block;
+                    vertical-align: bottom;
+                    overflow: hidden;
+                    text-overflow: ellipsis;"><b>${sender}</b></span>
+                    <span>${subject}</span> 
+                    <span style="float: right; margin-right: 8px;">${time_sent}</span>`
         document.querySelector('#emails-view').append(div);
       })
     });
@@ -88,8 +114,25 @@ function display_email(id, mailbox) {
       timesent = email.timestamp;
       body = email.body;
       const div = document.createElement('div');
-      // div.className = 'form-group';
-      div.innerHTML = 'From: ' + sender + '<br>Recipients: ' + recipients + '<br>Subject: ' + subject + '<br>Time: ' + timesent + '<br>Body: ' + body;
+      div.innerHTML = `
+          <div style="margin: 8px;">
+              <span style="width: 64px; display: inline-block;"><b>From:</b></span>
+              <input style="width: 90%;" disabled value="${sender} [${timesent}]">
+          </div>
+   
+          <div style="margin: 8px;">
+              <span style="width: 64px; display: inline-block;"><b>To:</b> </span>
+              <input style="width: 90%;" disabled value="${recipients}">
+          </div>
+          <div style="margin: 8px;">
+              <span style="width: 64px; display: inline-block;"><b>Subject:</b> </span>
+              <input style="width: 90%;" disabled value="${subject}">
+          </div>
+          <div style="margin: 8px;">
+              <span style="width: 64px; display: inline-block; vertical-align: top;"><b>Email:</b></span>
+              <textarea style="width: 90%; height: 50vh" disabled placeholder="${body}"></textarea>
+          </div><br>
+          `;
       document.querySelector('#display_email').append(div);
 
       if (mailbox === 'inbox') {
@@ -98,7 +141,12 @@ function display_email(id, mailbox) {
         button_reply.addEventListener('click', function () {
           reply_email(email);
         });
+        button_reply.style.backgroundColor = '#808080';
+
         document.querySelector('#display_email').append(button_reply);
+
+
+
         const button_archive = document.createElement('button');
         button_archive.innerHTML = 'Archive';
         button_archive.addEventListener('click', function () {
